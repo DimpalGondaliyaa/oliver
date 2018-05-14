2f3b2728f4f84a52385d9453545e346674ebfd81
RudraMeswaniya <rud.webrud@gmail.com>
2018-05-14 19:10:18 +0530

    home oage

---
+++ application/views/home.php
@@ -0,0 +1,172 @@
+<div class="main-box">
+    <div class="top-box">
+        <div class="row">
+            <div class="max-width">
+        <h4>Wixicode sécurise vos ventes entre particuliers en quelques clics !</h4>
+        <h5>Fonctionne avec tous les sites et journaux de petites annonces !</h5>
+            </div>
+        </div>
+    </div>
+
+    <div class="secondbox">
+        <div class="serchbox">
+            <div class="srchbox-container">
+                <div class="row">
+                    <div class="col s12 m4">
+                      <div class="s-box">
+                          <img src="<?php echo base_url(); ?>html/images/transaction.png" class="responsive-img">
+                          <h5>Transactions sécurisées</h5>
+                      </div>
+                  </div>
+                  <div class="col s12 m4">
+                      <div class="s-box">
+                          <img src="<?php echo base_url(); ?>html/images/des.png" class="responsive-img">
+                          <h5>Gestion des litiges</h5>
+                      </div>
+                  </div>
+                  <div class="col s12 m4">
+                      <div class="s-box">
+                          <img src="<?php echo base_url(); ?>html/images/paiments.png" class="responsive-img">
+                          <h5>Paiements vérifiés</h5>
+                      </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="x-box">
+        <div class="row">
+            <div class="detailBox">
+                <div class="x-box-container">
+                    <div class="col s12 m6">
+                        <div class="xBox">
+                            <div class="ttl-row">
+                                <div class="col s12 m6"><h3>Acheteur</h3></div>
+                                <div class="col s12 m6"><span class="right-txt">0€</span></div>
+                            </div>
+                            <div class="x-list">
+                                <ul class="home-testi owl-carousel">
+                                    <li class="item">
+                                        <p><img src="<?php echo base_url(); ?>html/images/check-mark.png"> &nbsp;Achetez l’esprit tranquille</p>
+                                        <p><img src="<?php echo base_url(); ?>html/images/check-mark.png"> &nbsp;Votre argent n’est remis au vendeur <br>
+                                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  qu’une fois le bien réçu et conforme</p>
+                                        <p><img src="<?php echo base_url(); ?>html/images/check-mark.png"> &nbsp;Gestion des litiges</p>
+                                        <p><img src="<?php echo base_url(); ?>html/images/check-mark.png"> &nbsp;100% gratuit</p>
+                                   </li> 
+                                </ul>
+                                <p class="right r-text">En savoir plus</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col s12 m6">
+                        <div class="xBox">
+                            <div class="ttl-row" style="height: 100px;">
+                                <div class="col s12 m5"><h3>Vendeur</h3></div>
+                                <div class="col s12 m7"><span class="x-left col s12 m12">6,90€* ttc </span>
+                                <span class="seulement-box col s12 m12">seulement</span></div>
+                            </div>
+                            <div class="x-list">
+                                <ul class="home-testi owl-carousel">
+                                    <li class="item">
+                                        <p><img src="<?php echo base_url(); ?>html/images/check-mark.png"> &nbsp;Générez vos Wixicodes <span style="color: red;">  gratuitement</span> </p>
+                                        <p><img src="<?php echo base_url(); ?>html/images/check-mark.png"> &nbsp;Paiements vérifiés </p>
+                                        <p><img src="<?php echo base_url(); ?>html/images/check-mark.png"> &nbsp;Gestion des litiges</p>
+                                        <p><img src="<?php echo base_url(); ?>html/images/check-mark.png"> &nbsp;Tarif unique</p><br>
+                                   </li> 
+                                </ul>
+                                <div class="col s12 m12">
+                                <label class="col s12 m6 lbl-left">*Hors frais bancaires applicables pour les réglements PayPal ou CB</label>
+                                <label class="col s12 m6"><p class="right r-text">En savoir plus</p></label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="list-detail-conatiner">
+        <div class="row">
+            <div class="max-width">
+                <div class="list-title">
+                    <h3>Découvrez la révolution <span><b>Wixicode</b></span></h3>
+                </div>
+                <div class="list-box">
+                   <ul>
+                      <li><i class="fas fa-circle"></i> &nbsp; Créez votre compte gratuitement</li>
+                      <li><i class="fas fa-circle"></i> &nbsp; Renseignez la fiche de l’article que vous vendez</li>
+                      <li><i class="fas fa-circle"></i> &nbsp; Récupérez le Wixicode associé à cette fiche</li>
+                      <li><i class="fas fa-circle"></i> &nbsp; Diffusez le Wixicode dans le texte de vos petites annonces</li>
+                  </ul>
+                </div>
+                <div class="p-text">
+                    <p>Et voilà, votre article peut être immédiatement acheté sur Wixicode.fr <br>Il suffit à l’acheteur de saisir le Wixicode pour accéder à la fiche de votre article.</p>
+                    <p>Vous pouvez créer autant de fiches articles que vous le souhaitez et cela <span>GRATUITEMENT</span> <br>
+                    Vous ne payez les frais Wixicode qu’en cas de vente.</p>
+                </div>
+                <div class="rendze-container">
+                    <div class="rendze-box">
+                        <p class="valign-wrapper"><i class="material-icons">keyboard_arrow_right</i> <span>Rendez-vous ici pour voir un exemple de transaction sécurisée avec Wixicode</span></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="sweetbox-container">
+        <div class="row">
+            <div class="max-width">
+                <div class="col s12 m4">
+                    <div class="swt-box">
+                        <div class="box-1">
+                        <h3>créez votre compte gratuitement</h3>
+                        </div>
+                        <div class="box-footer">
+                            <h5>Acheteur</h5>
+                            <h6>découvrez vos avantages</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m4">
+                    <div class="swt-box">
+                        <div class="box-2">
+                        <h3>Générer vos Wixicode gratuitement</h3>
+                        </div>
+                        <div class="box-footer">
+                            <h5>Vendeur</h5>
+                            <h6>découvrez vos avantages</h6>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s12 m4">
+                    <div class="swt-box">
+                        <div class="box-3">
+                        <h3>Gestion des litiges</h3>
+                        </div>
+                        <div class="box-footer">
+                            <h5>Litiges</h5>
+                            <h6>découvrez vos avantages</h6>
+                        </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+        </div>
+
+    <div class="bottom-box">
+        <div class="row">
+            <div class="max-width">
+                <div class="tagline">
+                    <h4>créez votre compte et générez vos wikicode gratuitement des maintenant</h4>
+                </div>
+                <div class="center">
+                    <button type="button" class="btn bnt-flat btn-sky">Incription</button>
+                </div>
+                <div>
+                    <p class="b-tg">Si Vous avez des questions consultez notre <span>FAQ</span> ou <span>Contactez-nous</span></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ application/views/includes/header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="msapplication-tap-highlight" content="no">
+    <title>CodeIgniter - <?php echo $pageTitle; ?></title>
+    <!-- CSS-->
+    <link href="<?php echo base_url(); ?>html/css/style.css" rel="stylesheet">
+    <link href="http://fonts.googleapis.com/css?family=Inconsolata" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
+            
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i&amp;subset=latin-ext" rel="stylesheet">
+
+    <!-- Font-sccs --><!-- 
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous"> -->
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
+
+    <script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="<?php echo base_url(); ?>html/js/script.js"></script>
+    <?php foreach($stylesheet as $fileName){ ?>
+    <link href="<?php echo base_url(); ?>html/css/<?php echo $fileName; ?>" rel="stylesheet">
+    <?php } ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+</head>
+
+    <header>
+         <nav>
+            <div class="nav-wrapper">
+              <a href="#" class="brand-logo"><img src="<?php echo base_url(); ?>html/images/logo.png" class="responsive-img
+                "></a>
+              <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="sass.html">Sass</a></li>
+                <li><a href="badges.html">Components</a></li>
+                <li><a href="collapsible.html">JavaScript</a></li>
+              </ul>
+            </div>
+          </nav>
+    </header>
+<body>
